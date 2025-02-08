e68f87670843163938f6ea263aae4dbe2cf7f6b3
JUAN PABLO OTERO FERNANDEZ <oterofernandezjuan@gmail.com>
2025-02-07 22:37:42 -0500

    Add files via upload

---
+++ tienda.java
@@ -0,0 +1,44 @@
+public class TIENDA {
+	private String nombre() {
+		return null;
+	}
+	private String dirección() {
+		return null;
+	}
+	private int telefono() {
+		return 0;
+	}
+	private List<Producto> productos() {
+		return null;
+	}
+
+    public class Clase_Producto {
+        private int Codigo;
+        private Date Fecha_expiracion;
+        private String Nombre_Fabricante;
+        private int Cantidad_Inventario;
+        private TipoProductos Tipo_producto;
+    
+        public TipoProductos tipo() {
+            return null;
+        }
+        public double precio_Unitario() {
+            return 0;
+        }
+        public String nombreProducto() {
+            return null;
+        }
+        public double Calcular_valorTotal() {
+            return 0;
+        }
+    
+    }
+    public class clase_TipoProducto {
+
+        private String Nombre_producto;
+        private String Descripcion;
+        public void Mostrar_info() {
+    
+        }
+    }
+}
