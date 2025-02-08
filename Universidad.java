0e52f97f9858f65e4deb5f6d77974944920b4e05
JUAN PABLO OTERO FERNANDEZ <oterofernandezjuan@gmail.com>
2025-02-08 10:37:24 -0500

    Add files via upload

---
+++ Universidad.java
@@ -0,0 +1,17 @@
+public class Universidad {
+
+	private String Nombre;
+
+	private String Nombre_rector;
+
+	private String Nombre_ciudad;
+
+	public void Gestion_Facultad() {
+
+	}
+
+	public void Actualizar_datos() {
+
+	}
+
+}
